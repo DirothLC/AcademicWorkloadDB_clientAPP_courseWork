8c9d0a01200c9a7ac22cdd928d6df59bd30fc36b
Kutsinas Darius <dkuc222@gmail.com>
2025-02-16 16:27:58 +0500

    added authorization-view alter AuthorizationController

--- src/main/java/kz/kstu/kutsinas/course_project/db/academic_workload/controllers/AuthorizationController.java
+++ src/main/java/kz/kstu/kutsinas/course_project/db/academic_workload/controllers/AuthorizationController.java
@@ -2,13 +2,16 @@ package kz.kstu.kutsinas.course_project.db.academic_workload.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class AuthorizationController {
     @FXML
-    private Label welcomeText;
+    private TextField loginField;
+    @FXML
+    private TextField passwordField;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onSignInButtonClick() {
+
     }
 }
